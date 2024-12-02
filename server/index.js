f6dc555c70964e9a6895321fb79a7fa6228d2477
Karthik Acharya <karthik.ach2608@gmail.com>
2024-12-02 18:56:46 +0530

    added cart model-route

--- server/index.js
+++ server/index.js
@@ -1,29 +1,36 @@
-dotenv = require('dotenv').config()
-const express = require('express')
+dotenv = require("dotenv").config();
+const express = require("express");
 
-const UserRouter = require('./routes/user.route')
-const connection = require('./config/db')
-const ProductRouter = require('./routes/product.route')
-const PORT = process.env.PORT || 3000
+const cors = require("cors");
+const UserRouter = require("./routes/user.route");
+const connection = require("./config/db");
+const ProductRouter = require("./routes/product.route");
+const CartRouter = require("./routes/cart.route");
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use('/user', UserRouter)
+app.use(express.json());
+
+app.use("/user", UserRouter);
 app.use("/product", ProductRouter);
+app.use("/cart", CartRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "server working fine" });
+});
 
-app.get("/", (req, res)=>{
-    res.status(200).json({ msg: "server working fine" });
-})
-
-
-app.listen(PORT, async()=>{
-    try {
-        await connection
-        console.log("server is running and DB is connected")
-    } catch (error) {
-        console.log("error occured while starting server")
-    }
-})
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("server is running and DB is connected");
+  } catch (error) {
+    console.log("error occured while starting server");
+  }
+});
---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const userAuthentication = (req, res, next) => {
+  const token = req.headers.authorization.split("")[1];
+
+  if (token) {
+    jwt.verify(token, process.env.SecretKey, (err, decoded) => {
+      if (decoded) {
+        req.body.userID = decoded.userID;
+        next();
+      } else {
+        res.status(403).send({ message: "User Authorization Error" });
+      }
+    });
+  } else {
+    res.status(403).send({ message: "Invalid Token" });
+  }
+};
+
+
+module.exports = userAuthentication;
