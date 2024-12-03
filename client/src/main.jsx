7f0ae13f0ce0f3de11600ebe67f63d69ca46434d
Karthik Acharya <karthik.ach2608@gmail.com>
2024-12-03 20:43:09 +0530

    added store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <ChakraProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </ChakraProvider>
 );
