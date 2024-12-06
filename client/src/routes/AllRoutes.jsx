0b26e4bf1ce97c2126b1c531e4741ae45c3ae686
Karthik Acharya <karthik.ach2608@gmail.com>
2024-12-06 14:44:34 +0530

    added routes

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Product from "../pages/Product";
+import Products from "../pages/Products";
+import PrivateRoutes from "./PrivateRoutes";
+import CartPage from "../pages/CartPage";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/products/:id" element={<Products />} />
+      <Route
+        path="/cart"
+        element={
+          <PrivateRoutes>
+            <CartPage />
+          </PrivateRoutes>
+        }
+      />
     </Routes>
   );
 };
